a056e6d01933a38af57f6b59a2f2efaacec73018
lnguyen87 <luc_nguyen@outlook.com>
2021-11-27 10:19:11 -0700

    linked 9am block to html

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,11 +1,17 @@
+// Get date and time from moment.js and display when page loads
+var today = moment().format("LLLL");
+$("#currentDay").html(today);
+console.log(today);
+
 // var workDay = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 var currentTime = parseInt(moment().format("HH"));
 console.log(currentTime);
 
 
-// Get date and time from moment.js and display when page loads
-var today = moment().format("LLLL");
-$("#currentDay").html(today);
+var $text9 = $("#text9");
+console.log(text9);
+text9.textContent = "testing";
+
 
 
 // check if current time is passed 9am-5pm
@@ -19,7 +25,7 @@ $("textarea").each(function() {
         $(this).addClass("future");
     }
 });
-    
+
 
 // $(".saveBtn").on("click", function() {
 //     var userTask = $(this).siblings("textarea").hour();
