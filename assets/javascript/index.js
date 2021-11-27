8e12560bf5d6eef2d761ce0caf67df1279e2c972
lnguyen87 <luc_nguyen@outlook.com>
2021-11-27 09:46:55 -0700

    added if statement to all timeblocks to format depending on current time vs time of day

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,27 +1,31 @@
-var currentTime = moment().format("HH");
+// var workDay = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+var currentTime = parseInt(moment().format("HH"));
 console.log(currentTime);
 
-var workDay = [9, 10, 11, 12, 13, 14, 15, 16, 17]
-console.log(workDay);
 
 // Get date and time from moment.js and display when page loads
 var today = moment().format("LLLL");
 $("#currentDay").html(today);
 
 
-function currentTimeTracker() {
-    // // check if current time is passed 9am-5pm
-    // for (var i = 0; i < workDay.length; i++) {
-        if (currentTime > workDay[0]) {
-            $("#9am").addClass("past");
-            console.log(currentTime);
-            console.log(workDay[0]);
-        } else if (currentTime === workDay[0]) {
-            $("#9am").addClass("present");
-        } else {
-            $("#9am").addClass("future");
-        }
-    };
+// check if current time is passed 9am-5pm
+$("textarea").each(function() {
+    var name = parseInt($(this).attr("name"));
+    if (name < currentTime) {
+        $(this).addClass("past");
+    } else if (name === currentTime) {
+        $(this).addClass("current");
+    } else {
+        $(this).addClass("future");
+    }
+});
     
-currentTimeTracker();
 
+// $(".saveBtn").on("click", function() {
+//     var userTask = $(this).siblings("textarea").hour();
+//     var timeblockHour
+// })
+
+// function loadTimeblock () {
+//     $("#9").val(localStorage.getItem("9"));
+// }
