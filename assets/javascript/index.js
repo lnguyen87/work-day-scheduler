309804da4cca649bcb7b50533c06a5869eb830a8
lnguyen87 <luc_nguyen@outlook.com>
2021-11-26 19:37:14 -0700

    added containers for work day hours

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,5 +6,24 @@
 // today = mm + "/" + dd + "/" + yyyy;
 // console.log(today);
 
+var workDay = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+console.log(workDay);
+
+// Get date and time from moment.js and display when page loads
 var today = moment().format("LLLL");
 $("#currentDay").html(today);
+
+
+function currentTimeTracker() {
+    var currentTime = moment().format("HH");
+    console.log(currentTime);
+    var textArea = $("textarea");
+        
+    // check if current time is passed 9am-5pm
+    for (var i = 0; i < workDay.length; i++) {
+        if (currentTime > workDay[i]) {
+            $("#9am").addClass("past");
+        }
+    }
+};
+
